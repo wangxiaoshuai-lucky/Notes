e1c04aa2a4b528b6167a78252f1ce3b4e9666a87
1510460325@qq.com <1510460325@qq.com>
2018-12-18 08:29:59 +0800

    添加对mysql和锁的部分知识点

--- src/cn/wzy/Lock/Main.java
+++ src/cn/wzy/Lock/Main.java
@@ -7,8 +7,38 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Main {
 
+	static ReadWriteLock rwLock = new ReentrantReadWriteLock();
+	static Lock readLock = rwLock.readLock();
+	static Lock write = rwLock.writeLock();
 	public static void main(String[] args) {
-		Lock lock = new ReentrantLock();
-		ReadWriteLock rwLock = new ReentrantReadWriteLock();
+		for (int i = 0; i < 50; i++) {
+			if (i % 2 < 0) {
+				new Thread(()->{
+					write.lock();
+					try {
+						System.out.println(Thread.currentThread().getId() + "is starting to write the file");
+						Thread.sleep(500);
+						System.out.println(Thread.currentThread().getId() + "is completing to write the file");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} finally {
+						write.unlock();
+					}
+				}).start();
+			} else {
+				new Thread(()->{
+					readLock.lock();
+					try {
+						System.out.println(Thread.currentThread().getId() + "is starting to read the file");
+						Thread.sleep(500);
+						System.out.println(Thread.currentThread().getId() + "is completing to read the file");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} finally {
+						readLock.unlock();
+					}
+				}).start();
+			}
+		}
 	}
 }
