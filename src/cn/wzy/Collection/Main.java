79bfe230ba237b630f283a4bff7f2b5572ca4192
1510460325@qq.com <1510460325@qq.com>
2018-12-18 10:03:12 +0800

    添加阻塞队列的理解

--- src/cn/wzy/Collection/Main.java
+++ src/cn/wzy/Collection/Main.java
@@ -2,10 +2,13 @@ package cn.wzy.Collection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<>();
+	public static void main(String[] args) throws InterruptedException {
+		LinkedBlockingDeque<Integer> queue = new LinkedBlockingDeque<>(12);
+		queue.put(12);
+		queue.take();
 	}
 }
