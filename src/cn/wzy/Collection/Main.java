891500044ada9f49141b25bc45d0eebf4b6e30c1
1510460325@qq.com <1510460325@qq.com>
2019-01-13 17:41:07 +0800

    添加异常处理

--- src/cn/wzy/Collection/Main.java
+++ src/cn/wzy/Collection/Main.java
@@ -1,45 +1,10 @@
 package cn.wzy.Collection;
 
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.LinkedBlockingDeque;
-
 public class Main {
-
-
-	public static void sort(int[] target, int low, int high) {
-		if (low >= high) {
-			return;
-		}
-		int i = low, j = high;
-		int tmp = target[i];
-
-		while (i < j) {
-			while (i < j) {
-				if (target[j] < tmp) {
-					target[i] = target[j];
-					break;
-				}
-				j--;
-			}
-			while (i < j) {
-				if (target[i] > tmp) {
-					target[j] = target[i];
-					break;
-				}
-				i++;
-			}
-			target[i] = tmp;
-		}
-		sort(target, low, i - 1);
-		sort(target, i + 1, high);
-	}
-
 	public static void main(String[] args) {
-		ConcurrentHashMap<String, Integer> hashMap = new ConcurrentHashMap<>();
-		hashMap.put("asdf",11);
-		hashMap.size();
+		Integer a = new Integer("12");
+		Integer b = new Integer("12");
+		System.out.println(a.equals(b));
+		System.out.println(a == b);
 	}
 }
