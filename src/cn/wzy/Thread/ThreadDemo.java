856e88210b5b72c70c567f91d4114cd7b27d8a57
1510460325@qq.com <1510460325@qq.com>
2019-03-04 18:18:07 +0800

    最长公共子序列

--- src/cn/wzy/Thread/ThreadDemo.java
+++ src/cn/wzy/Thread/ThreadDemo.java
@@ -1,27 +1,64 @@
 package cn.wzy.Thread;
 
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+
+import static java.lang.Math.max;
 
 public class ThreadDemo {
 	public static void main(String[] args) {
-		ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-		for (int i = 0; i < 30; i++) {
-			final int index = i;
-			if (i == 0) {
-				executor.scheduleAtFixedRate(()->
-						System.out.println("现在时间 : " + System.currentTimeMillis() + " , 我在运行.")
-				,0,2, TimeUnit.SECONDS);
-			} else {
-				executor.execute(()->{
-					System.out.println("我不是延迟任务，我优先级最高，我是任务" + index);
-					try {
-						Thread.sleep(500);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNext()) {
+			String s = scanner.next();
+			String a = scanner.next();
+			char[] str1 = s.toCharArray();
+			char[] str2 = a.toCharArray();
+			int[][] dp = new int[str1.length + 1][str2.length + 1];
+			int result = 0;
+			for (int i = 0; i < str1.length + 1; i++) {
+				for (int j = 0; j < str2.length + 1; j++) {
+					if (i == 0 || j == 0) {
+						dp[i][j] = 0;
+					}else if (str1[i - 1] == str2[j - 1]) {
+						dp[i][j] = dp[i - 1][j - 1] + 1;
+					} else {
+						dp[i][j] = 0;
 					}
-				});
+					if (dp[i][j] > result)
+						result = dp[i][j];
+				}
+			}
+//		for (int i = 0; i < dp.length; i++) {
+//			for (int j = 0; j < dp[0].length; j++) {
+//				System.out.print(dp[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+			System.out.println(result);
+//		System.out.println(lcs(s,a));
+		}
+
+	}
+	public static int lcs(String str1, String str2) {
+		int len1 = str1.length();
+		int len2 = str2.length();
+		int c[][] = new int[len1+1][len2+1];
+		for (int i = 0; i <= len1; i++) {
+			for( int j = 0; j <= len2; j++) {
+				if(i == 0 || j == 0) {
+					c[i][j] = 0;
+				} else if (str1.charAt(i-1) == str2.charAt(j-1)) {
+					c[i][j] = c[i-1][j-1] + 1;
+				} else {
+					c[i][j] = max(c[i - 1][j], c[i][j - 1]);
+				}
 			}
 		}
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[0].length; j++) {
+				System.out.print(c[i][j] + " ");
+			}
+			System.out.println();
+		}
+		return c[len1][len2];
 	}
 }
