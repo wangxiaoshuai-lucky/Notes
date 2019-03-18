0a2f425398218b0c9983e8048bdf386f2cbfb288
1510460325@qq.com <1510460325@qq.com>
2019-03-18 15:17:33 +0800

    硬币dp问题

--- src/cn/wzy/Collection/Main.java
+++ src/cn/wzy/Collection/Main.java
@@ -1,23 +1,35 @@
 package cn.wzy.Collection;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.locks.ReentrantLock;
+
+import static java.lang.Math.min;
 
 public class Main {
+	static int[] cons = new int[]{1, 3, 4, 5};
+
+	static int ans(int n) {
+		int[] dp = new int[n + 1];
+		for (int i = 0; i <= n; i++)
+			dp[i] = 99999;
+		dp[0] = 0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < 4 && i - cons[j] >= 0; j++) {
+				dp[i] = min(dp[i - cons[j]] + 1, dp[i]);
+			}
+		}
+		int ans = dp[n];
+		int now = n;
+		while ((ans--) != 0) {
+			for (int i = 0; i < 4 && now - cons[i] >= 0; i++) {
+				if (dp[now - cons[i]] == dp[now] - 1) {
+					System.out.println(ans + ": " + cons[i]);
+					now -= cons[i];
+					break;
+				}
+			}
+		}
+		return dp[n];
+	}
+
 	public static void main(String[] args) {
-		Child child = new Child();
-		((Base)child).say();//bases
-		(child).say();//child
-		HashMap<String,String> map = new HashMap<>();
-		map.put("asdf","asdf");
-		AtomicInteger integer = new AtomicInteger();
-		integer.get();
-		AtomicReference<String> reference = new AtomicReference<>();
-		reference.get();
-		Thread thread = new Thread();
-		thread.interrupt();
-		Thread.interrupted();
-		new ReentrantLock();
+		System.out.println(ans(7));
 	}
 }
