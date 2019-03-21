25e240f68ef5026c5fffc66a5f86ae806c595574
1510460325@qq.com <1510460325@qq.com>
2019-03-21 10:25:43 +0800

    添加futureTask的理解

--- src/cn/wzy/exception/Main.java
+++ src/cn/wzy/exception/Main.java
@@ -1,6 +1,10 @@
 package cn.wzy.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
 	public static void main(String[] args) throws RuntimeException,NullPointerException {
 		RuntimeException exception = new NullPointerException();
 		exception = new RuntimeException();
